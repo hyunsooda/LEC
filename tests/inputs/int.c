647a8af35ba2642f7f5e2f430122fe6c1ce7dfca
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2023-03-16 00:54:46 +0900

    [Test] Test files updated

--- tests/inputs/int.c
+++ tests/inputs/int.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 int main() {
-    char SIZE = 5;
+    char SIZE = 15;
+    int v1 = 7;
+    int v2 = 8;
     int* intarr = (int*)malloc(sizeof(int) * SIZE);
     intarr[4] = 123;
-    printf("%d\n", intarr[7]);
+    printf("%d\n", intarr[v1 * v2]);
     exit(1);
 }
