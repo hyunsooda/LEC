be6f72681c3400dfe19187c8158d53e01bbbac17
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2023-04-23 16:47:41 +0900

    [Run] Added `run.sh`

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+input=$1
+output=$2
+
+function modify_lec_global_string() {
+    LEC_DEFINED_ANSI_RED_GLOBAL_VAR='LEC_ANSI_RED_g767akzwihq04k3frbvijvx2l5gdn0sk'
+    LEC_DEFINED_ANSI_WHITE_GLOBAL_VAR='LEC_ANSI_WHITE_g767akzwihq04k3frbvijvx2l5gdn0sk'
+
+    ANSI_RED='1B[31m'
+    ANSI_WHITE='1B[37m'
+
+    # Ref: https://superuser.com/questions/590630/sed-how-to-replace-line-if-found-or-append-to-end-of-file-if-not-found
+    RED_COLOR="unnamed_addr constant [6 x i8] c\""\\\\$ANSI_RED\\\\00"\""
+    sed -i '/^@'$LEC_DEFINED_ANSI_RED_GLOBAL_VAR""'/{h;s/=.*/= '"$RED_COLOR"'/}' $output
+    WHITE_COLOR="unnamed_addr constant [6 x i8] c\""\\\\$ANSI_WHITE\\\\00"\""
+    sed -i '/^@'$LEC_DEFINED_ANSI_WHITE_GLOBAL_VAR""'/{h;s/=.*/= '"$WHITE_COLOR"'/}' $output
+}
+
+rm -rf $output
+stack run -- lec -i $input -o $output
+modify_lec_global_string
+lli $output
