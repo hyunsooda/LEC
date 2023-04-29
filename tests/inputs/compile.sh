eb766d4366f526ef4e283a5ff0d7bfe6d2764a8b
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2023-04-29 21:58:42 +0900

    [Test] Added two 1-d static array examples

--- tests/inputs/compile.sh
+++ tests/inputs/compile.sh
@@ -2,3 +2,5 @@
 
 clang-15 -g -S -emit-llvm const.c
 clang-15 -g -S -emit-llvm int.c
+clang-15 -g -S -emit-llvm static-arr.c
+clang-15 -g -S -emit-llvm static-arr2.c
---
+++ tests/inputs/static-arr2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    const int size = 5;
+    int arr[size];
+    printf("%d\n", arr[13]);
+}
