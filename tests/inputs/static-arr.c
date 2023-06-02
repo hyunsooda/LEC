9027a8b0aa8748ad372a31a2049e9d3642a51d58
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2023-06-03 03:13:14 +0900

    [] Moved to other directories

--- tests/inputs/static-arr.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int arr[10] = {1,2,3,4,5,6,7,8,9,10}; // TODO: Add `const int size = 5;`
-    printf("%d\n", arr[12]);
-}
--- tests/inputs/static-arr2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    const int size = 5;
-    int arr[size];
-    printf("%d\n", arr[13]);
-}
