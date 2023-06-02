4cb318d10312c7bdd18b59145efe98710aa00b66
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2023-06-03 00:25:01 +0900

    [Pass] (1) Entirely refactored (2) Added code reference and function name reference intiializations

--- src/Pass.hs
+++ src/Pass.hs
@@ -5,7 +5,9 @@ module Pass where
 import Prelude hiding (mod, log)
 
 import Type
+import CFG
 import Backend
+import Metadata
 
 import System.Console.ANSI
 import Control.Monad.State hiding (void)
@@ -14,12 +16,18 @@ import qualified Data.Map as M
 import Data.List (isPrefixOf)
 
 import qualified LLVM.AST as AST
-import qualified LLVM.AST.Global as G
+import qualified LLVM.AST.Global as AST
 
 import LLVM.Internal.Context as LLVM
 import LLVM.Internal.Module as LLVM
 import LLVM.IRBuilder.Monad as LLVM
 import LLVM.IRBuilder.Module as LLVM
+
+import Control.Monad.Identity
+import System.IO.Unsafe
+
+instance MonadIO Identity where
+  liftIO = Identity . unsafePerformIO
 
 data FileTyp = BC | LL deriving Eq
 
@@ -27,13 +35,21 @@ printErr :: String -> IO ()
 printErr err =
   setSGR [SetColor Foreground Vivid Red] >> print err >> setSGR [Reset]
 
+initState :: PassState
+initState = StateMap { intMap = M.empty
+                     , debugMap = M.empty
+                     , sourceMap = M.empty
+                     , taintMap = M.empty
+                     , codeMap = M.empty
+                     , demangledFuncMap = M.empty
+                     }
+
 runPass :: Pass a -> PassInput -> AST.Module -> Bool -> IO (AST.Module, a)
 runPass pass input mod debug = do
-  let passInitState = StateMap { intMap = M.empty, debugMap = M.empty, sourceMap = M.empty, taintMap = M.empty }
   let irBuilderState = LLVM.emptyIRBuilder
   let modBuilderState = LLVM.emptyModuleBuilder
   let (((result, _), output), defs) = LLVM.runModuleBuilder modBuilderState $ do
-                              (\x -> evalRWST x input passInitState) $
+                              (\x -> evalRWST x input initState) $
                                 runIRBuilderT irBuilderState $ pass mod debug
 
   mapM_ (\log -> if "ERROR: " `isPrefixOf` log then printErr log else putStrLn log) output
@@ -41,42 +57,64 @@ runPass pass input mod debug = do
 
 analyze :: FileTyp -> FilePath -> String -> Bool -> IO ()
 analyze typ file outputPath debug = do
-  mod <- LLVM.withContext $ withModule typ
+  mod <- getModule typ file
   (mod', _) <- runPass outOfBoundChecker [] mod debug
   LLVM.withContext $ \ctx ->
-    LLVM.withModuleFromAST ctx mod' $ toFile typ
+    LLVM.withModuleFromAST ctx mod' $ mod2File typ
   where
-    withModule LL ctx = do
+    mod2File LL modl = LLVM.writeLLVMAssemblyToFile (LLVM.File outputPath) modl
+    mod2File BC modl = LLVM.writeBitcodeToFile (LLVM.File outputPath) modl
+
+getModule :: FileTyp -> FilePath -> IO AST.Module
+getModule fileTyp filePath = LLVM.withContext $ withModule fileTyp filePath
+  where
+    withModule LL file ctx = do
       fcts <- readFile file
       LLVM.withModuleFromLLVMAssembly ctx fcts LLVM.moduleAST
-    withModule BC ctx =
+    withModule BC file ctx =
       LLVM.withModuleFromBitcode ctx (LLVM.File file) LLVM.moduleAST
-    toFile LL modl = LLVM.writeLLVMAssemblyToFile (LLVM.File outputPath) modl
-    toFile BC modl = LLVM.writeBitcodeToFile (LLVM.File outputPath) modl
 
 outOfBoundChecker :: AST.Module -> Bool -> IRBuilderT Env ()
 outOfBoundChecker mod debug = do
   let defs = AST.moduleDefinitions mod
       funcNames = foldl getFuncNames [] defs
-  emitChecker funcNames
-  mapM_ iterDef (reverse defs)
+  mapM addCode defs
+  mapM addDemangledFnStr defs
+  mapM_ initDebugInfo defs
+  mapM_ emitBase defs
+  mapM_ iterDef defs
+
   ppSM
-  emitGlobalAnsiStr >> outOfBoundErrLogFormat >> pure ()
+  emitGlobalAnsiStr >> outOfBoundErrAssertFmt >> mapKeyExistAssertFmt
+  emitBoundAssertion funcNames
+  emitMapUninitChecker
+  pure ()
   where
-    iterDef (AST.GlobalDefinition f@(G.Function {})) = do
-      updateIntMap f
-      instrumented <- instrument f
-      LLVM.emitDefn instrumented
-    iterDef d@(AST.MetadataNodeDefinition nodeId mdNode) = do
+    iterDef d@(AST.GlobalDefinition f@(AST.Function {..})) =
+      when (not . null $ basicBlocks) $ do
+        updateIntMap f                -- step1 (pre-analysis)
+        instrumented <- instrument f  -- step2 (emitting final code)
+        LLVM.emitDefn instrumented
+    iterDef _ = pure ()
+
+    -- emit everything except for the functions that have basic blocks
+    emitBase d@(AST.GlobalDefinition f@(AST.Function {..})) =
+      if null basicBlocks then LLVM.emitDefn d else pure () 
+    emitBase d = LLVM.emitDefn d
+
+    initDebugInfo d@(AST.MetadataNodeDefinition nodeId mdNode) =
       modify $ \sm -> sm { debugMap = M.insert nodeId mdNode (debugMap sm) }
-      LLVM.emitDefn d
-    iterDef d = LLVM.emitDefn d
+    initDebugInfo _ = pure ()
 
-    emitChecker funcNames = do
+    emitBoundAssertion funcNames = do
       _ <- defBoundChecker
-      unless ("\"exit\"" `elem` funcNames) $ emitLibcExit >> pure ()
+      unless ("exit" `elem` funcNames) $ emitLibcExit >> pure ()
 
-    getFuncNames acc (AST.GlobalDefinition (G.Function {..})) = getName name : acc
+    emitMapUninitChecker = do
+      countFns <- findMapCountFns 
+      mapM_ defMapUninitChecker countFns
+  
+    getFuncNames acc (AST.GlobalDefinition (AST.Function {..})) = getName name : acc
     getFuncNames acc _ = acc
 
     -- print state map
