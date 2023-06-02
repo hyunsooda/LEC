ae000260c8430d3719d39cbda43b2adee203face
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2023-06-03 00:24:11 +0900

    [Type] Added `codeMap` and `demangeldFuncMap` environment for IR reference and function name reference

--- src/Type.hs
+++ src/Type.hs
@@ -2,9 +2,11 @@ module Type where
 
 import Control.Monad.RWS hiding (void)
 import qualified Data.Map as M
+import Data.ByteString.Short (ShortByteString)
 
 import qualified LLVM.AST as AST
 import qualified LLVM.AST.Operand as AST
+import qualified LLVM.AST.Global as AST
 
 import LLVM.IRBuilder.Monad as LLVM
 import LLVM.IRBuilder.Module as LLVM
@@ -14,10 +16,12 @@ data StateMap = StateMap { intMap :: M.Map AST.Name Integer
                          , debugMap :: M.Map AST.MetadataNodeID AST.MDNode
                          , sourceMap :: M.Map AST.Name AST.DIVariable
                          , taintMap :: M.Map AST.Name [AST.Name]
+                         , codeMap :: M.Map AST.Name AST.Global -- <function name, Global.Function>
+                         , demangledFuncMap :: M.Map String AST.Name -- <demangled function name, mangled function name>
                          } deriving Show
 
 type PassInput = [String]
 type PassState = StateMap
 type PassOutput = [String]
-type Env = (RWST PassInput PassOutput PassState ModuleBuilder)
+type Env = RWST PassInput PassOutput PassState ModuleBuilder
 type Pass a = AST.Module -> Bool -> IRBuilderT Env a
