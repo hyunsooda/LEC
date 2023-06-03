fcf7991df16797a0c50d72ce498e035ffa9cc6df
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2023-06-03 12:10:57 +0900

    [Test] Added uninitialized map access checker test

--- tests/Test.hs
+++ tests/Test.hs
@@ -6,17 +6,20 @@ import Test.Tasty.Hspec
 import OOB
 import CFGSpec
 import MetadataSpec
+import UninitMapCheckerSpec
 
 import System.Process
 
+-- Hide shell outputs
 compile :: IO ()
-compile = callCommand "cd tests/inputs; ./compile.sh"
+compile = callCommand "cd tests/inputs; ./compile.sh 1> /dev/null 2> /dev/null"
 
 main :: IO ()
 main = do
   compile
 
-  oobBasicTest <- oobBasic
+  oobBasicTest <- oobBasic "basic" "tests/inputs/OOB"
+  uninitMapAccessChekcerTest <- uninitMapAccessCheckerBasic "basic" "tests/inputs/uninit-map-access"
   cfgBasicTest <- cfgBasic
   cfgTermTest <- cfgTerm
   typeLookupTest <- typLookupBasic
@@ -24,6 +27,7 @@ main = do
 
   defaultMain (testGroup "All tests" [
                   testGroup "OOB test" [oobBasicTest]
+                , testGroup "Uninitialized map access test" [uninitMapAccessChekcerTest]
                 , testGroup "CFG test" [cfgBasicTest, cfgTermTest]
                 , testGroup "Metadata test" [typeLookupTest, demangleFnNameTest]
               ])
